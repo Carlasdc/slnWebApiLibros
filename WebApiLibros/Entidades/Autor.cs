60ad87bd375a847e2bb0a985b7a0495609a2523f
carlasdc <carlamarielsandoval@gmail.com>
2022-11-16 18:59:29 -0300

    Agregar archivos de proyecto.

---
+++ WebApiLibros/Entidades/Autor.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApiLibros.Entidades
+{
+    public class Autor
+    {
+        public Autor(int id, string nombre)
+        {
+            Id = id;
+            Nombre = nombre;
+        }
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
